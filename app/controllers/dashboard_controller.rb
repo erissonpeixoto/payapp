064b6c2cd3e562a1ad058c43b31544a00cc8a790
Erisson Peixoto <erisson.peixoto@useblu.com.br>
2023-02-02 10:02:30 -0300

    Improvements

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,15 +4,18 @@ class DashboardController < ApplicationController
   def index
     
     salary = ::Configuration.where(user: current_user).last.try(:salary).to_f
-    total_value_last_payment = Payment.where(user: current_user).order(:payday).last.try(:value).to_f
+    user_payment = Payment.where(user: current_user).order(:payday).last
+    total_value_last_payment = user_payment.try(:value).to_f
     balance = salary - total_value_last_payment
-    
+
+    # last user payment
+    @last_user_payment = user_payment
+
     #chart 1
     @chart_pay_and_balance = {"Pago #{number_to_currency(total_value_last_payment, unit: "R$ ", separator: ",", delimiter: ".")}" => total_value_last_payment, "Saldo #{number_to_currency(balance, unit: "R$ ", separator: ",", delimiter: ".")}" => balance}
     @color_chart_pay_and_balance = (balance < 0) ? ["#2E86C1","red"] : ["#2E86C1","#3FBB31"]
 
     #chart 2
-    user_payment = Payment.where(user: current_user).order(:payday).last
     @payment_of_expenses_by_category = []
     if user_payment.present?
       @payment_of_expenses_by_category = user_payment.expense_payments.joins(:category).group('categories.name').sum(:value).map {|category, value| {category => value.to_f}}.inject(:merge!)
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -8,17 +8,14 @@ class Payment < ApplicationRecord
 
   accepts_nested_attributes_for :expense_payments, reject_if: :all_blank, allow_destroy: true
 
-  after_save :total_value
+  attribute :status, :integer, default: Proc.new { Payment.statuses["aberto"] }
 
+  after_save :total_value
+  
   enum status: {
     aberto: 0,
     concluido: 1
   }
-
-  def total_value
-    self.value = self.expense_payments.inject(0){|sum, ep| sum + ep.value }
-    self.update_column(:value, self.value)
-  end
 
   def expenses_fixed_not_used 
     Expense.where(user: user).where(fixed: true).where.not(id: [expense_ids])
@@ -27,5 +24,12 @@ class Payment < ApplicationRecord
   def balance_in_percentage
     salary = ::Configuration.where(user: user).last.try(:salary).to_f
     balance = ((self.value.to_f * 100)/salary).round(2)
+  end
+
+  private
+
+  def total_value
+    self.value = self.expense_payments.inject(0){|sum, ep| sum + ep.value }
+    self.update_column(:value, self.value)
   end
 end
